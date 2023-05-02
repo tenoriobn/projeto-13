2515825b21b1b3e27b840ab35ca561751f9a6c13
Bruno Tenório <tenoriobn@gmail.com>
2023-05-02 15:39:30 -0300

    Criando base do projeto

---
+++ assets/js/1-script.js
@@ -0,0 +1,9 @@
+/*
+    Implementações:
+        - Veja o layout ideal para o conteúdo, dependendo do tamanho da tela do dispositivo
+        - Veja os estados de foco para todos os elementos interativos na página
+        - Veja a barra do dia atual destacada em uma cor diferente das outras barras
+        - Visualize o gráfico de barras e passe o mouse sobre as barras individuais para ver os valores corretos para cada dia
+        - **Bônus**: Use o arquivo de dados JSON fornecido para dimensionar dinamicamente as barras no gráfico
+*/
+
