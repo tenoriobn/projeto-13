6ae9e207e8ccbd730b5929acf79b1b2faa163209
Bruno Tenório <tenoriobn@gmail.com>
2023-05-09 16:09:39 -0300

    Retirando linha das extremidades do gráfico

--- assets/js/1-script.js
+++ assets/js/1-script.js
@@ -1,10 +1,10 @@
 /*
     Implementações:
         - Veja o layout ideal para o conteúdo, dependendo do tamanho da tela do dispositivo**
-        - Veja os estados de foco para todos os elementos interativos na página
-        - Veja a barra do dia atual destacada em uma cor diferente das outras barras
         - Visualize o gráfico de barras e passe o mouse sobre as barras individuais para ver os valores corretos para cada dia**
-        - **Bônus**: Use o arquivo de dados JSON fornecido para dimensionar dinamicamente as barras no gráfico**
+        - Use o arquivo de dados JSON fornecido para dimensionar dinamicamente as barras no gráfico**
+        - Veja os estados de foco para todos os elementos interativos na página
+        - Veja a barra do dia atual destacada em uma cor diferente das outras barras-----
 */
 
 import graphicValues from "./2-graphicValues.js";
@@ -17,7 +17,8 @@ const canvasBarChart = new Chart(graphicBar, {
         labels: [],
         datasets: [{
             data: [],
-            borderRadius: 3,
+            borderRadius: 5,
+            borderSkipped: false,
             backgroundColor: ['hsl(10, 79%, 65%)'],
             hoverBackgroundColor: 'hsl(10, 100%, 76%)',
         }]
@@ -71,11 +72,14 @@ const canvasBarChart = new Chart(graphicBar, {
             x: {
                 grid: {
                     display: false,
-                }
+                },
+                border: {
+                    display: false,
+                },
             },
             y: {
                 display: false,
-            }
+            },
         },
         
     }
